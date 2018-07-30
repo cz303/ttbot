598a51ccbb0017496f8b2b6700489af375641d07
TheGreatMarat <38732447+TheGreatMarat@users.noreply.github.com>
2018-07-30 20:27:14 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-API_TOKEN = "679020958:AAHa7kzPJ8RxDDSbsMGCPR6kci2H55C5HME"
+API_TOKEN = "663742265:AAF8WPAu8i5RcU2fcznbWij5Jr0D64-NbBI"
 database_name = "info.db"
 shelve_name = "shelve.db"
 
